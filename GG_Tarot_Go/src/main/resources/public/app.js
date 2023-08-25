a64b22c729a03a4d6570ca9b824a4727cf669f02
Joe <Jhuntenburg@gmail.com>
2023-08-25 16:05:21 -0400

    all cards added to public folder

--- GG_Tarot_Go/src/main/resources/public/app.js
+++ GG_Tarot_Go/src/main/resources/public/app.js
@@ -1,4 +1,5 @@
-const API_URL = `http://localhost:8080`
+// const API_URL = `http://localhost:8080`
+const API_URL = `http://192.168.3.39:8080`
 
 function fetchCardsData() {
     fetch(`${API_URL}/api/cards`)
